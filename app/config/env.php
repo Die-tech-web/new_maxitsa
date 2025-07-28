774d8500b4871ce6b53ebb1b53177b1631e16d11
Die-tech-web <dieniang32@gmail.com>
2025-07-28 19:58:18 +0000

    correction env

--- app/config/env.php
+++ app/config/env.php
@@ -1,9 +1,19 @@
 <?php
 
+// $dotenv = Dotenv\Dotenv::createImmutable('../');
+// $dotenv->load();
+
+// define("URL", $_ENV["URL"]);
+// define("DB_USERNAME", $_ENV["DB_USERNAME"]);
+// define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
+// define( "DSN", $_ENV["DSN"]);
+
+
+
 $dotenv = Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
-define("URL", $_ENV["URL"]);
-define("DB_USERNAME", $_ENV["DB_USERNAME"]);
-define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
-define( "DSN", $_ENV["DSN"]);
+define("URL", $_ENV["URL"] ?? '');
+define("DB_USERNAME", $_ENV["DB_USERNAME"] ?? '');
+define("DB_PASSWORD", $_ENV["DB_PASSWORD"] ?? '');
+define("DSN", $_ENV["DSN"] ?? '');
